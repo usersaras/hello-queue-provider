a72b22e1305fd7aad2f9782f4ab6120cddc34c26
saras-sth <saras.shrestha@globalyhub.com>
2023-11-29 20:49:14 +0545

    setup: Initial project setup

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import configure from './routers';
+import express from "express";
+import configure from "./routers";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,5 +9,5 @@ configure(app);
 console.log(`Attempting to run server on port ${port}`);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
